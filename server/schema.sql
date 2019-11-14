b404e518de62a466ea20d04b10e4c7b77df66469
kwillard95 <kwillard95@gmail.com>
2019-11-13 20:35:47 -0800

    Implements DB schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,30 +1,35 @@
-CREATE DATABASE chat;
+DROP DATABASE IF EXISTS Chat;
+CREATE DATABASE Chat;
 
-USE chat;
+USE Chat;
 
 CREATE TABLE users (
-  -- id INT AUTO_INCREMENT
-  -- PRIMARY KEY(id),
+  id INT AUTO_INCREMENT,
+  PRIMARY KEY(id),
   username CHAR(20)
 );
 
--- CREATE TABLE rooms (
---   id INT AUTO_INCREMENT
---   PRIMARY KEY(id),
---   roomname CHAR(20)
--- );
--- CREATE TABLE friends (
---   FOREIGN KEY(user1) REFERENCES users(id),
---   FOREIGN KEY(user2) REFERENCES users(id)
--- );
+CREATE TABLE rooms (
+  id INT AUTO_INCREMENT,
+  PRIMARY KEY(id),
+  roomname CHAR(20)
+);
+CREATE TABLE friends (
+  user1 INT,
+  FOREIGN KEY(user1) REFERENCES users(id),
+  user2 INT,
+  FOREIGN KEY(user2) REFERENCES users(id)
+);
 
--- CREATE TABLE messages (
---   id INT AUTO_INCREMENT
---   PRIMARY KEY(id),
---   FOREIGN KEY(user) REFERENCES users(id),
---   FOREIGN KEY (room) REFERENCES rooms(id),
---   content CHAR(100)
--- );
+CREATE TABLE messages (
+  id INT AUTO_INCREMENT,
+  PRIMARY KEY(id),
+  user INT,
+  FOREIGN KEY(user) REFERENCES users(id),
+  room INT,
+  FOREIGN KEY (room) REFERENCES rooms(id),
+  content CHAR(100)
+);
 
 /* Create other tables and define schemas for them here! */
 
