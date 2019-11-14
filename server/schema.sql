55fe07355a29191c0ec3e3c89443bd94dd250e9b
kwillard95 <kwillard95@gmail.com>
2019-11-13 17:32:56 -0800

    Builds chatterbox schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,29 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  -- id INT AUTO_INCREMENT
+  -- PRIMARY KEY(id),
+  username CHAR(20)
 );
+
+-- CREATE TABLE rooms (
+--   id INT AUTO_INCREMENT
+--   PRIMARY KEY(id),
+--   roomname CHAR(20)
+-- );
+-- CREATE TABLE friends (
+--   FOREIGN KEY(user1) REFERENCES users(id),
+--   FOREIGN KEY(user2) REFERENCES users(id)
+-- );
+
+-- CREATE TABLE messages (
+--   id INT AUTO_INCREMENT
+--   PRIMARY KEY(id),
+--   FOREIGN KEY(user) REFERENCES users(id),
+--   FOREIGN KEY (room) REFERENCES rooms(id),
+--   content CHAR(100)
+-- );
 
 /* Create other tables and define schemas for them here! */
 
